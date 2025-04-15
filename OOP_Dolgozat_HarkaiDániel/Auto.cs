a3aecf30d9a35d98b14c2d1e7a7afba9a05dd6a6
dani20010924@gmail.com <dani20010924@gmail.com>
2025-04-15 10:15:03 +0200

    Add project files.

---
+++ OOP_Dolgozat_HarkaiDániel/Auto.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Dolgozat_HarkaiDániel
+{
+    internal class Auto
+    {
+        public string Marka { get; set; }
+        public string Tipus { get; set; }
+        public int GyartasiEv { get; set; }
+        public double Fogyasztas { get; set; }
+        public double MegtettKm { get; set; }
+
+        public Auto(string marka, string tipus, int gyartasiEv, double fogyasztas, double megtettKm)
+        {
+            Marka = marka;
+            Tipus = tipus;
+            GyartasiEv = gyartasiEv;
+            Fogyasztas = fogyasztas;
+            MegtettKm = megtettKm;
+        }
+
+        public int eletkor()
+        {
+            return DateTime.Now.Year - GyartasiEv;
+        }
+
+        public double uzemanyag_felhasznalas()
+        {
+            return Math.Round(MegtettKm * Fogyasztas / 100, 2);
+        }
+
+        public double atlagfogyasztas_kmre()
+        {
+            return Fogyasztas / 100;
+        }
+
+        public void adatkiiras()
+        {
+            Console.WriteLine("Autó adatai:");
+            Console.WriteLine($"Márka: {Marka}");
+            Console.WriteLine($"Típus: {Tipus}");
+            Console.WriteLine($"Gyártási év: {GyartasiEv}");
+            Console.WriteLine($"Életkor: {eletkor()} év");
+            Console.WriteLine($"Fogyasztás: {Fogyasztas} l/100km");
+            Console.WriteLine($"Megtett kilométer: {MegtettKm} km");
+            Console.WriteLine($"Összesen felhasznált üzemanyag: {uzemanyag_felhasznalas()} liter");
+            Console.WriteLine($"Átlagos fogyasztás 1 km-re: {atlagfogyasztas_kmre()} liter/km");
+        }
+
+    }
+}
---
+++ OOP_Dolgozat_HarkaiDániel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Dolgozat_HarkaiDániel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto toyota = new Auto("Toyota", "Corolla", 2015, 6.5, 152000);
+            toyota.adatkiiras();
+
+            Console.ReadKey();
+        }
+    }
+}
